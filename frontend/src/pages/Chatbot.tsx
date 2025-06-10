03545cbb9bc9fe253724208ecf768c614735c9cd
Hoang Anh <288hoanganh@gmail.com>
2025-06-10 21:03:33 +0700

    feat: Implement chatbot components with user query and response handling, and enhance input functionality

--- frontend/src/pages/Chatbot.tsx
+++ frontend/src/pages/Chatbot.tsx
@@ -1,35 +1,36 @@
 import type { JSX } from "react";
 import "@styles/pages/Chatbot.css";
-import { UserIcon } from "@/components/common/Icons";
+import UserQuery from "@/features/chatbot/components/UserQuery";
+import Response from "@/features/chatbot/components/Response";
+import Input from "@/features/chatbot/components/Input";
 
 const Chatbot = (): JSX.Element => {
   return (
     <div className="chatbot">
       <div className="chatbot__body">
         <div className="chatbot__body-container">
-          <div className="chatbot__body-bot__logo">{UserIcon}</div>
           <div className="chatbot__body-content">
-            <div className="user">
-              <div className="user__query">
-                <p>
-                  AIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXc
-                </p>
-              </div>
-            </div>
-            <div className="bot">
-              <div className="bot__name">eMastery</div>
-              <div className="bot__response">
-                AIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXcAIzaSyCEGcXqBcRgWDS5Moyg2PNjWA--B7EvxXc
-              </div>
-            </div>
+            <UserQuery />
+            <Response />
+            <UserQuery />
+            <Response />
+            <UserQuery />
+            <Response />
+            <UserQuery />
+            <Response />
           </div>
         </div>
       </div>
       <div className="chatbot__footer">
-        <div className="chatbot__footer-input__area">
-          {/* <input type="text" /> */}
+        <div className="chatbot__footer-container">
+          <Input />
         </div>
-        <div className="chatbot__footer-infomation"></div>
+        <div className="chatbot__footer-infomation">
+          <p>
+            eMastery có thể mắc sai sót, vì vậy, hãy xác minh các câu trả lời
+            của eMastery
+          </p>
+        </div>
       </div>
     </div>
   );
