be8a0d3b360184f94778f7b141ff8ad1e47f840f
Hoang Anh <288hoanganh@gmail.com>
2025-11-22 03:39:23 +0700

    feat: enhance CORS configuration and validation in serverless.ts; update TypeScript settings in tsconfig.json

--- backend-nestjs/src/serverless.ts
+++ backend-nestjs/src/serverless.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 export async function createApp() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://e-mastery.vercel.app',
+      /\.vercel\.app$/,
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.setGlobalPrefix('api');
+
   await app.init();
   return app.getHttpAdapter().getInstance();
 }
